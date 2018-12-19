5e75dc8395f75406c7482c222e797c372ecff28b
Harry Soer <gelo.soer@gmail.com>
2018-12-19 13:56:10 +0800

    Added basic header styling

--- App.js
+++ App.js
@@ -6,46 +6,47 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import styled from "styled-components/native";
+// const instructions = Platform.select({
+//   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+//   android:
+//     "Double tap R on your keyboard to reload,\n" +
+//     "Shake or press menu button for dev menu"
+// });
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const Header = styled.View`
+    align-items: center;
+    background: #41d46a;
+    height: 300px;
+    justify-content: center;
+    margin-bottom: 30px;
+`
+
+const Title = styled.Text`
+    color: #ffff;
+    font-size: 40px;
+`;
+
+const Description = styled.Text`
+    color: #ffff;
+    font-size: 18px;
+    margin: 30px 20px;
+    text-align: center;
+`
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-        Products Grid
-Here you're sure to find a bargain on some of the finest ascii available to purchase. Be sure to peruse our selection of ascii faces in an exciting range of sizes and prices.</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Header >
+        <Title>Products Grid</Title>
+        <Description>
+          Here you're sure to find a bargain on some of the finest
+          ascii available to purchase. Be sure to peruse our selection of ascii
+          faces in an exciting range of sizes and prices.
+        </Description>
+      </Header>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
